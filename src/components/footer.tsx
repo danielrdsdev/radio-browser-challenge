d6426a8bdcbf244e3f2f7109ccd2a08b154427b0
danielrdsdev <danielrds.dev@gmail.com>
2024-04-30 14:27:47 -0300

    feat: add Footer component

---
+++ src/components/footer.tsx
@@ -0,0 +1,9 @@
+export const Footer = () => {
+	return (
+		<footer className="py-8 border-t">
+			<p className="text-center text-sm text-muted-foreground">
+				&copy; {new Date().getFullYear()} Radio Browser
+			</p>
+		</footer>
+	);
+};

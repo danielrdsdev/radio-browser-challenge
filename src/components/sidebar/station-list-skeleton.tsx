afcb09b12150809e458aaca5e5f49881ab0d1199
danielrdsdev <danielrds.dev@gmail.com>
2024-05-01 14:29:58 -0300

    feat: add skeleton-list-skeleton component

---
+++ src/components/sidebar/station-list-skeleton.tsx
@@ -0,0 +1,14 @@
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { Skeleton } from "@/components/ui/skeleton";
+
+export const StationListSkeleton = () => {
+	return (
+		<ScrollArea className="px-4 h-[calc(100dvh-10rem)]">
+			<div className="space-y-4">
+				{[...Array(10)].map((_, i) => (
+					<Skeleton key={`${i + 1}`} className="h-20 w-full" />
+				))}
+			</div>
+		</ScrollArea>
+	);
+};
